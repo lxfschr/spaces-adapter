60f6fdd12151fe782fc0f1c12535e596fbdcc72f
Joel Brandt <joelrbrandt@gmail.com>
2014-05-21 19:18:34 -0700

    initial scaffolding

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
-playground-api
-==============
+Playground JS API
+=================
+
+Development Setup
+-----------------
+
+1. Install [Node](http://nodejs.org/)
+2. Install [Bower](http://bower.io/)
+3. If you're inside a corporate firewall that disallows `git://` URLs, see the note below and act accordingly
+4. Clone this repo
+5. In the root directory of this repo, run `bower install`
+
+File URL caveat
+---------------
+
+For some reason, qunit doesn't work correctly from `file://` URLs. So, consider installing serf:
+
+1. `npm install -g serf`
+2. `serf -o` from the root of the repo
+
+Directories
+-----------
+
+Put code in `src/`.
+
+Put unit test specs in `test/spec/` and then add them to `test/testrunner.js`
+
+Running Tests
+-------------
+
+Open `test/index.html` in a web browser. 
+
+Notes
+-----
+
+To make bower work in places where `git://` URLs don't work (e.g. inside a corporate firewall), run this git command:
+
+```bash
+git config --global url."https://".insteadOf git://
+```
+
+To undo that, run:
+
+```bash
+git config --global --unset url."https://".insteadOf
+```
---
+++ test/spec/hello-test.js
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2014 Adobe Systems Incorporated. All rights reserved.
+ *  
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"), 
+ * to deal in the Software without restriction, including without limitation 
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+ * and/or sell copies of the Software, and to permit persons to whom the 
+ * Software is furnished to do so, subject to the following conditions:
+ *  
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *  
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+ * DEALINGS IN THE SOFTWARE.
+ * 
+ */
+
+/* global asyncTest, expect, ok, equal, start */
+
+define(function (require) {
+    "use strict";
+
+    var hello = require("hello");
+
+    asyncTest("asyncDouble doubles a number", function () {
+        expect(2);
+
+        hello.asyncDouble(3, function (err, n) {
+            ok(!err, "callback received no error");
+            equal(n, 6, "3 * 2 = 6");
+            start();
+        });
+    });
+
+    asyncTest("asyncDouble fails on argument error", function () {
+        expect(1);
+
+        hello.asyncDouble("hi", function (err) {
+            ok(err, "callback received error");
+            start();
+        });
+    });
+
+});
