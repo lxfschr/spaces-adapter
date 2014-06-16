801fb321b06952b50635852e63df6fa5ad648296
Joel Brandt <joelrbrandt@gmail.com>
2014-06-16 07:09:24 -0700

    remove xdlib example from jshint/jscs

--- Gruntfile.js
+++ Gruntfile.js
@@ -44,7 +44,7 @@ module.exports = function (grunt) {
                 "lib/**/*.js",
                 "test/**/*.js",
                 "examples/**/*.js",
-                "!examples/designshop/**/*.js"
+                "!examples/xdlib/**/*.js"
             ]
         },
         jscs: {
