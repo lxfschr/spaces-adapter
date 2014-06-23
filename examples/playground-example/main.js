54f24871010ec1aa483d7b8255c45f7463641c28
Barkin Aygun <volfied@gmail.com>
2014-06-23 16:04:47 -0700

    Add a adapter.get caller to UI for quick testing

--- examples/playground-example/main.js
+++ examples/playground-example/main.js
@@ -1,24 +1,24 @@
 /*
  * Copyright (c) 2014 Adobe Systems Incorporated. All rights reserved.
- *  
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a
- * copy of this software and associated documentation files (the "Software"), 
- * to deal in the Software without restriction, including without limitation 
- * the rights to use, copy, modify, merge, publish, distribute, sublicense, 
- * and/or sell copies of the Software, and to permit persons to whom the 
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
  * Software is furnished to do so, subject to the following conditions:
- *  
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- *  
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
- * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
  * DEALINGS IN THE SOFTWARE.
- * 
+ *
  */
 
 /* global require */
@@ -41,7 +41,7 @@ define(function (require) {
 
     var _setup = function () {
         adapter.log("Version: " + playground.version);
-        
+
         // set up new layer button handler
         var buttonNewLayer = document.getElementsByClassName("button-new-layer")[0];
         if (buttonNewLayer) {
@@ -59,6 +59,21 @@ define(function (require) {
                     newLayerName = newLayerNameField.value;
                 }
                 layerManager.duplicateLayer(null, newLayerName);
+            };
+        }
+
+        var buttonAdapterGet = document.getElementsByClassName("button-adapter-get")[0];
+        var textGetInput = document.getElementsByName("get-input")[0];
+        var textOutput = document.getElementsByName("get-output")[0];
+        if (buttonAdapterGet) {
+            buttonAdapterGet.onclick = function () {
+                var getInput = "$Dcmn";
+                if (textGetInput.value !== "") {
+                    getInput = textGetInput.value;
+                }
+                adapter.get(getInput).then(function (value) {
+                    textOutput.value = JSON.stringify(value, null, "  ");
+                });
             };
         }
 
